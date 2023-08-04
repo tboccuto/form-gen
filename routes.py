5293aace833028a2a37dc21fd4ca0bb0e8f6d74b
tboccuto <tboccuto@upenn.edu>
2023-08-04 16:27:45 -0400

    removing uneeded comments

--- routes.py
+++ routes.py
@@ -1,20 +1,10 @@
 from flask import render_template, request, redirect, url_for
 import forms
 
-""" 
-Generate the login form (using flask) for the index.html page, where you will 
-enter a new user. The form itself is created in forms.py. 
-The index() route method is called from app.py
-"""
 def index():
     form = forms.LoginForm()
     return render_template('index.html', form=form)
 
-""" 
-Retrieve all the rows from the database and return them.
-All the data will be displayed on entire_database.html file.
-The view_database() route method is called from app.py
-"""
 def view_database():
     from app import get_all_rows_from_table
     rows = get_all_rows_from_table()
@@ -24,18 +14,14 @@ def view_database():
 def modify_database(the_id ,modified_category):
     if request.method == 'POST':
         from app import modify_data
-        # Get data from the form on database page
         user_input = request.form[modified_category]
-        # modify the row from the database
         modify_data(the_id, modified_category, user_input)
-        # redirect back to the database page
         return redirect(url_for('view_database'))
     return redirect(url_for('index'))
 
 def delete(the_id):
     if request.method == 'POST':
         from app import delete_data
-        # if the checkbox was selected (for deleting entire row)
         if 'remove' in request.form:
             delete_data(the_id)
     return redirect(url_for('view_database'))
