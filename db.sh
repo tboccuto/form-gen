3f373698fb04a310ae2200b7715fc51e8ad6b546
tboccuto <tboccuto@upenn.edu>
2023-08-06 16:57:48 -0400

    move User obj to app.py read form values dynamically and remove imports

--- db.sh
+++ db.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
-#Example usage ./db.sh name phone email job > user_class.py
+#Example usage ./db.sh name phone email job > user_class.py gernerates user model.
 if [ $# -lt 1 ]; then
   echo "Usage: $0 field1 [field2 field3 ...]"
   exit 1
 fi
 table_name="Users"
-class_code="from flask_sqlalchemy import SQLAlchemy\n"
-class_code+="from app import *\n"
-class_code+="db = SQLAlchemy(app)\n"
-class_code+="class ${table_name}(db.Model):\n"
+class_code+="class User(db.Model):\n"
 class_code+="    __tablename__ = '${table_name}'\n"
 class_code+="    id = db.Column(db.Integer, primary_key=True)\n"
 for field in "$@"; do
   class_code+="    ${field} = db.Column(db.String(50))\n"
 done
-echo -e "$class_code"
-ret=$(ls -t | head -n1)
-echo "from ${ret%.*} import *" | cat - app.py > temp && mv temp app.py
+#echo -e "$class_code" >> app.py
+awk -v class_code="$class_code" '/^db = SQLAlchemy\(app\)$/ {print; printf "%s", class_code; next} 1' app.py > temp_app.py
+mv temp_app.py app.py
--- user_class.py
+++
@@ -1,8 +0,0 @@
-class Users(db.Model):
-    __tablename__ = 'Users'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    phone = db.Column(db.String(50))
-    email = db.Column(db.String(50))
-    job = db.Column(db.String(50))
-
