402044e0b35c8f5f25a8682b5dd97cfb1be7bef8
tboccuto <boccutothomas@outlook.com>
2023-08-08 23:37:48 -0400

    fresh state

--- db.sh
+++ db.sh
@@ -7,36 +7,41 @@ fi
 imports="from app import *\n"
 imports+="from flask_sqlalchemy import SQLAlchemy\n"
 imports+="import os\n"
-
-
-
-table_name="Users"
-class_code+="class $table_name(db.Model):\n"
-class_code+="    __tablename__ = '${table_name}'\n"
+class_name="${1%?}"$''
+class_code+="class $class_name(db.Model):\n"
+table_name="${1%?}"$''
+class_code+="    __tablename__ = '$1'\n"
 class_code+="    id = db.Column(db.Integer, primary_key=True)\n"
-for field in "$@"; do
-  class_code+="    ${field} = db.Column(db.String(50))\n"
+for ((i = 2; i <= $#; i++)); do
+   field="${!i}" 
+  class_code+="    ${field} = db.Column(db.String(50))\n" 
 done
 
+
 class_code+="def insert_data(**kwargs):\n"
-class_code+="    db.session.add($table_name(**kwargs))\n"
+class_code+="    db.session.add($class_name(**kwargs))\n"
 class_code+="    db.session.commit()\n"
 class_code+="\n"
 class_code+="def modify_data(the_id, col_name, user_input):\n"
-class_code+="    filter = ($table_name).query.filter_by(id=the_id).first()\n"
+class_code+="    filter = $class_name.query.filter_by(id=the_id).first()\n"
 class_code+="    setattr(user, col_name, user_input)\n"
 class_code+="    db.session.commit()\n"
 class_code+="\n"
 class_code+="def delete_data(the_id):\n"
-class_code+="    el = User.query.filter_by(id=the_id).first()\n"
+class_code+="    el = $class_name.query.filter_by(id=the_id).first()\n"
 class_code+="    db.session.delete(el)\n"
 class_code+="    db.session.commit()\n"
 class_code+="\n"
 class_code+="def get_all_rows_from_table():\n"
-class_code+="    return $table_name.query.all()\n"
+class_code+="    return $class_name.query.all()\n"
 
 #echo -e "$class_code" >> app.py
-echo -e "$imports" >> temp.py
-echo -e "$class_code" >> temp.py
+echo -e "$imports" >> model.py
+echo -e "db = SQLAlchemy(app)" >> model.py
+echo -e "$class_code" >> model.py
+echo -e "db.create_all()" >> model.py
+#app_import="from model import *"
+sed -i '1 i\from model import *' app.py
+#echo -e "$app_import" >> app.py
 #awk -v class_code="$class_code" '/^db = SQLAlchemy\(app\)$/ {print; printf "%s", class_code; next} 1' app.py > temp_app.py
 #mv temp_app.py app.py
--- routes.py
+++ routes.py
@@ -1,5 +1,3 @@
-import forms
-from forms import *
 from flask import render_template, request, redirect, url_for
 
 def index():
@@ -7,14 +5,14 @@ def index():
     return render_template('index.html', form=form)
 
 def view_database():
-    from app import get_all_rows_from_table
+    from model import get_all_rows_from_table
     rows = get_all_rows_from_table()
     
     return render_template('entire_database.html', rows=rows)
 
 def modify_database(the_id ,modified_category):
     if request.method == 'POST':
-        from app import modify_data
+        from model import modify_data
         user_input = request.form[modified_category]
         modify_data(the_id, modified_category, user_input)
         return redirect(url_for('view_database'))
@@ -22,13 +20,13 @@ def modify_database(the_id ,modified_category):
 
 def delete(the_id):
     if request.method == 'POST':
-        from app import delete_data
+        from model import delete_data
         if 'remove' in request.form:
             delete_data(the_id)
     return redirect(url_for('view_database'))
 
 def submitted():
-    from app import insert_data
+    from model import insert_data
     if request.method == 'POST':
         insert_data(**{k: v for k,v in request.form.items() if k !='csrf_token'})
     return render_template('submitted.html')
